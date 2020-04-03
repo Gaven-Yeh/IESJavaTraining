b2d4255a282566cb2e7cb46264db33b6c1690b80
Gaven-Yeh <gaven.yeh@accenture.com>
2020-04-03 11:04:33 +0800

    Simple flyway demo

---
+++ flyway/src/main/resources/db/migration/V3__add_employees.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee (name, email, date_of_birth) values ('Jake', 'jake@accenture.com', '1996-05-22 00:00:01');
+INSERT INTO employee (name, email, date_of_birth) values ('Amy', 'amy@accenture.com', '1997-01-02 00:00:01');
+INSERT INTO employee (name, email, date_of_birth) values ('Raymond', 'raymond@accenture.com', '1966-11-15 00:00:01');
